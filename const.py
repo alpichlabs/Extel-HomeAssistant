b13fd7617333c12535d0c48599efdf17bb925961
Jason Pretavoine <128550353+jasonpretavoine@users.noreply.github.com>
2026-01-16 23:30:49 +0100

    Update DOMAIN and add default headers

--- const.py
+++ const.py
@@ -1,2 +1,11 @@
-DOMAIN = "extel_gate"
+DOMAIN = "extel_umii"
+CONF_DEVICE_ID = "device_id"
 CONF_GATE_ID = "gate_id"
+
+# Headers vus dans tes captures
+DEFAULT_HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "User-Agent": "Umii/85 CFNetwork/3860.200.71",
+    "Accept-Encoding": "gzip"
+}
